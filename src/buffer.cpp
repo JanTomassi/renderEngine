4eb3b1b35548903a5493ca1e37a570f6151e97e1
JanTomassi <tomassijan@gmail.com>
2023-09-13 15:18:36 +0200

    docs(buffer): moving documentation from src to header file

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -2,22 +2,12 @@
 #include "debug.hpp"
 
 using namespace JRE::glObject;
-/**
- * @brif Init a new buffer assiume
- * @param buffer_type set the type of the buffer
- */
-Buffer::Buffer (int buffer_type): type(buffer_type)
+
+Buffer::Buffer (int buffer_type) : type (buffer_type)
 {
   glGenBuffers (1, &m_RenderId);
 }
 
-/**
- * @brif set the opengl buffer data
- * @param type is the GL_{Type}_BUFFER
- * @param data pointer to the start of the data, data must be contiguse
- * @param size expressed in byte
- * @return if the call was successful
- */
 void
 Buffer::set_data (const void *data, unsigned int size) const
 {
@@ -25,23 +15,14 @@ Buffer::set_data (const void *data, unsigned int size) const
   glBufferData (type, size, data, GL_STATIC_DRAW);
 }
 
-/**
- * @brif delete the underling opengl buffer
- */
 Buffer::~Buffer () { glDeleteBuffers (1, &m_RenderId); }
 
-/**
- * @brif bind the opengl buffer from the context
- */
 void
 Buffer::bind () const
 {
   glBindBuffer (type, m_RenderId);
 }
 
-/**
- * @brif unbing the opengl buffer from the context
- */
 void
 Buffer::unbind () const
 {
--- src/buffer.hpp
+++ src/buffer.hpp
@@ -11,11 +11,34 @@ private:
   const int type;
 
 public:
+  /**
+   * @brif Init a new buffer assiume
+   * @param buffer_type set the type of the buffer
+   */
   Buffer (int buffer_type);
+
+  /**
+   * @brif set the opengl buffer data
+   * @param type is the GL_{Type}_BUFFER
+   * @param data pointer to the start of the data, data must be contiguse
+   * @param size expressed in byte
+   * @return if the call was successful
+   */
   void set_data (const void *data, unsigned int size) const;
+
+  /**
+   * @brif delete the underling opengl buffer
+   */
   ~Buffer ();
 
+  /**
+   * @brif bind the opengl buffer from the context
+   */
   void bind () const;
+
+  /**
+   * @brif unbing the opengl buffer from the context
+   */
   void unbind () const;
 };
 }
