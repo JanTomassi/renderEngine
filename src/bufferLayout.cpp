6d065dc1377def99d96bbf31d6887085d3c51f1f
JanTomassi <tomassijan@gmail.com>
2023-09-14 22:33:51 +0200

    fix(bufferLayout): append fun using wrong GL Type

--- src/bufferLayout.cpp
+++ src/bufferLayout.cpp
@@ -17,14 +17,14 @@ template <>
 void
 BufferLayout::append<GLuint> (uint32_t count)
 {
-  m_layers.push_back (Layer{ GL_FLOAT, count, false });
-  m_stride += count * Layer::get_size_of_type (GL_FLOAT);
+  m_layers.push_back (Layer{ GL_UNSIGNED_INT, count, false });
+  m_stride += count * Layer::get_size_of_type (GL_UNSIGNED_INT);
 }
 
 template <>
 void
 BufferLayout::append<GLbyte> (uint32_t count)
 {
-  m_layers.push_back (Layer{ GL_FLOAT, count, false });
-  m_stride += count * Layer::get_size_of_type (GL_FLOAT);
+  m_layers.push_back (Layer{ GL_BYTE, count, false });
+  m_stride += count * Layer::get_size_of_type (GL_BYTE);
 }
