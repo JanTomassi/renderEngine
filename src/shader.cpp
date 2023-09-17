eb2219c9223cd855efdb4593cdf117042b6d7321
JanTomassi <tomassijan@gmail.com>
2023-09-17 22:55:57 +0200

    feat(shader): GPU program wrapper
    Wrapping the GPU program creation, with a class that can handle the
    creation of all type of shader the API is compose of 2 majore function
    
    1. add_src: given the source file of the shader it compiled it and add
    it to program shader queue
    2. use_program: link to the programm all the shader and start using
    the program

---
+++ src/shader.cpp
@@ -0,0 +1,154 @@
+#include "shader.hpp"
+#include <algorithm>
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <glm/gtc/type_ptr.hpp>
+
+
+using namespace JRE::glObject;
+
+// Private functions
+
+uint32_t
+report_compile_error (uint32_t shader_id)
+{
+  int msg_length;
+  char *message;
+  glGetShaderiv (shader_id, GL_INFO_LOG_LENGTH, &msg_length);
+
+  message = static_cast<char *>(alloca (msg_length * sizeof (char))); // Allocate on the stack
+
+  glGetShaderInfoLog (shader_id, msg_length, &msg_length, message);
+
+  std::cout << "Failed to compile: " << std::endl;
+  std::cout << message << std::endl;
+
+  glDeleteShader (shader_id);
+  return 0;
+}
+
+uint32_t
+Shader::compile_shader (uint32_t type, std::string &source)
+{
+  uint32_t shader_id = glCreateShader (type);
+  const char *src = source.c_str ();
+  glShaderSource (shader_id, 1, &src, NULL);
+  glCompileShader (shader_id);
+
+  int res;
+  glGetShaderiv (shader_id, GL_COMPILE_STATUS, &res);
+  if (res == GL_FALSE)
+    {
+      report_compile_error(shader_id);
+      return 0;
+    }
+
+  return shader_id;
+}
+
+void
+report_link_error (const uint32_t program_id)
+{
+  int msg_length;
+  char *message;
+  glGetProgramiv (program_id, GL_INFO_LOG_LENGTH, &msg_length);
+  message = static_cast<char*>(alloca(msg_length * sizeof(char))); // Allocate ont he stack
+  
+  glGetProgramInfoLog (program_id, 1024, &msg_length, message);
+  std::cout << "Error in linking:" << std::endl;
+  std::cout << message << std::endl;
+}
+
+void
+Shader::link_to_shader ()
+{
+  for (size_t i = 0; i < m_compiled_shader_id.size (); i++)
+    glAttachShader (m_shader_program_id, m_compiled_shader_id[i]);
+
+  glLinkProgram (m_shader_program_id);
+
+  // Testo for linking error
+  int is_program_linked;
+  glGetProgramiv (m_shader_program_id, GL_LINK_STATUS, &is_program_linked);
+  if (is_program_linked != GL_TRUE)
+    report_link_error (m_shader_program_id);
+
+  glValidateProgram (m_shader_program_id);
+
+  for (size_t i = 0; i < m_compiled_shader_id.size (); i++)
+    glDetachShader (m_shader_program_id, m_compiled_shader_id[i]);
+}
+
+// Public funcitons
+
+Shader::Shader () : m_shader_program_id (glCreateProgram ()){};
+
+Shader::~Shader ()
+{
+  std::for_each (m_compiled_shader_id.cbegin (), m_compiled_shader_id.cend (),
+                 [] (uint32_t it) { glDeleteShader (it); });
+  glDeleteProgram (m_shader_program_id);
+}
+
+void
+file_to_string (std::ifstream &in, std::string &shader_string)
+{
+  std::string c;
+  std::getline (in, c);
+  if (c != "")
+    shader_string += c;
+  shader_string += '\n';
+}
+
+void
+Shader::add_src (uint32_t type, const std::string &src)
+{
+  std::ifstream in (src);
+  std::string shader_string;
+
+  if (!in.is_open ())
+    return; // Quit if the file can't be open or dosen't exist
+
+  while (!in.eof ())
+    file_to_string (in, shader_string);
+  shader_string += '\n';
+
+  uint32_t id = compile_shader (type, shader_string);
+  m_compiled_shader_id.push_back (id);
+
+  shader_string.clear ();
+  in.close ();
+}
+
+void
+Shader::use_program ()
+{
+  if (!is_shader_compiled)
+    link_to_shader ();
+  glUseProgram (m_shader_program_id);
+}
+
+uint32_t
+Shader::get_uniform_location (const std::string &name)
+{
+  return glGetUniformLocation (m_shader_program_id, name.c_str ());
+}
+
+void
+Shader::set_uniform_4f (const std::string &name, float a, float b, float c,
+                        float d)
+{
+  glUniform4f (get_uniform_location (name), a, b, c, d);
+}
+
+void
+Shader::set_uniform_mat_4f (const std::string &name, const glm::mat4 &value)
+{
+  glUniformMatrix4fv (get_uniform_location (name), 1, GL_FALSE,
+                      glm::value_ptr (value));
+}
---
+++ src/shader.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <string>
+#include <vector>
+
+
+#include <GL/glew.h>
+#include <glm/matrix.hpp>
+
+namespace JRE
+{
+namespace glObject
+{
+class Shader
+{
+private:
+  const uint32_t m_shader_program_id;
+  std::vector<uint32_t> m_compiled_shader_id;
+  bool is_shader_compiled = false;
+
+  uint32_t compile_shader (uint32_t type, std::string &source);
+
+  /**
+   * @brief Link compiled shader to program
+   */
+  void link_to_shader ();
+
+public:
+  /**
+   * @brief Create GPU program
+   */
+  Shader ();
+
+  /**
+   * @brief Delete shader and GPU program
+   */
+  ~Shader ();
+
+  /**
+   * @brief Add compiled version of the src file to the program queue
+   * @param type indicates what type of shade this is
+   * @param src path to source file on disk
+   */
+  void add_src (uint32_t type, const std::string &src);
+
+  /**
+   * @brief Compile if there are shader to compile and then start the program
+   */
+  void use_program ();
+
+  /**
+   * @brief Get shader uniform location from name
+   */
+  uint32_t get_uniform_location (const std::string &name);
+
+  /**
+   * @brief Set shader uniform of type float with 4 elements
+   */
+  void set_uniform_4f (const std::string &name, float a, float b, float c,
+                       float d);
+
+  /**
+   * @brief Set shader uniform of type float with a 4x4 matrix
+   */
+  void set_uniform_mat_4f (const std::string &name, const glm::mat4 &value);
+};
+}
+}
