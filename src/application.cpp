918b9c9f03d963fa94500e988c4c316b54a39f3b
JanTomassi <tomassijan@gmail.com>
2023-09-21 22:22:50 +0200

    style(application): formatting

--- src/application.cpp
+++ src/application.cpp
@@ -2,7 +2,7 @@
 int
 main (int argc, char **argv)
 {
-  JRE::WindowManager main_windows = JRE::WindowManager(640,480);
-  
-  return main_windows.start();
+  JRE::WindowManager main_windows = JRE::WindowManager (640, 480);
+
+  return main_windows.start ();
 }
