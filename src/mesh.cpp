dc1dc899d672a4b1b6372714f59c13709167633a
Jan Tomassi <tomassijan@gmail.com>
2024-02-12 19:14:52 +0100

    feat(mesh): impl load_mesh function

---
+++ src/mesh.cpp
@@ -0,0 +1,24 @@
+#include "mesh.hpp"
+#include <cstddef>
+#include <tuple>
+#include "buffer.hpp"
+#include "bufferLayout.hpp"
+#include "mesh_parser/obj_parser.hpp"
+#include "vertexArray.hpp"
+
+std::tuple<JRE::glObject::VertexArray, std::vector<JRE::Mesh::Index> >
+JRE::Mesh::load_mesh (const std::string &path)
+{
+  using namespace glObject;
+  Buffer data_buf (Buffer::buffer_types::attributes,
+                   Buffer::buffer_usage::static_draw);
+
+  auto [vertexs, layout, index]
+      = Loader::ObjParser ().get_vertex_and_index (path);
+
+  data_buf.set_data (vertexs.data (), sizeof (vertexs[0]), vertexs.size ());
+
+  VertexArray va (std::move (data_buf), std::move (layout));
+
+  return std::tuple (std::move (va), std::move (index));
+}
