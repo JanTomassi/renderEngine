0b8bee3b4c307a4063958956049b0857c5c237a0
Jan Tomassi <tomassijan@gmail.com>
2024-02-12 19:08:04 +0100

    feat(mesh): add load_mesh fn change c array to c++ std::array

--- src/mesh.hpp
+++ src/mesh.hpp
@@ -1,6 +1,11 @@
 #pragma once
+#include "bufferLayout.hpp"
+#include "vertexArray.hpp"
 #include <compare>
+#include <cstddef>
 #include <cstdint>
+#include <string>
+#include <array>
 
 namespace JRE
 {
@@ -11,9 +16,9 @@ namespace Mesh
  */
 struct Vertex
 {
-  float pos[3];
-  float nom[3];
-  float tex[2];
+  std::array<float, 3> pos;
+  std::array<float, 3> nom;
+  std::array<float, 2> tex;
 
   auto operator<=> (const Vertex &v) const = default;
 };
@@ -24,5 +29,12 @@ struct Index
 {
   uint32_t index;
 };
+
+/**
+ * @brief Load a mesh in memory
+ * @return A unique-id to identify the mesh or -1 for error
+ */
+std::tuple<JRE::glObject::VertexArray, std::vector<JRE::Mesh::Index> >
+load_mesh (const std::string &path);
 }
 }
